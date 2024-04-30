10f049a8a7791656ee21299d7543b89d973b79ea
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2024-04-30 13:39:15 +0300

    init

--- src/main/java/com/company/app/StarterConfiguration.java
+++ src/main/java/com/company/app/StarterConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy
 @ComponentScan
 @ConfigurationPropertiesScan
 public class StarterConfiguration {
